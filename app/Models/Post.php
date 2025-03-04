d229d272aff20c9cb30fff1eb93c1083a2cf320a
Arif <muhamadarifulinnuha@gmail.com>
2025-03-04 10:30:23 +0700

    learn about Model Factories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
+    use HasFactory;
     protected $table = 'articles';
     protected $fillable = ['title', 'author', 'slug', 'body'];
     protected $guarded = [];
