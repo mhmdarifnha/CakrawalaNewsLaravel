b6a87e40448fa72fb413bbb4efab378da17b3af5
Arif <muhamadarifulinnuha@gmail.com>
2025-03-16 19:55:56 +0700

    update href in single article page author href

--- resources/views/single-article.blade.php
+++ resources/views/single-article.blade.php
@@ -3,7 +3,7 @@
     <article class="pb-4 mb-4 w-full">
         <h2 class="text-xl font-bold">{{ $post['title'] }}</h2>
         <div class="flex items-center gap-1 text-sm text-gray-500">
-            <a href="/authors/{{ $post->author->id }}"
+            <a href="/authors/{{ $post->author->username }}"
                 class="hover:underline transition-all duration-150">{{  $post->author->name }}</a> | <p>Diposting pada:
                 {{ $post->created_at->diffForHumans() }}
             </p>
